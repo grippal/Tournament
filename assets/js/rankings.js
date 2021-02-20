7f02a15f202b03166458c271da3c84a2c535368c
grippal <lucagrip@yahoo.com>
2021-02-19 20:16:54 -0500

    Updated for week 6

---
+++ assets/js/rankings.js
@@ -0,0 +1,47 @@
+rankings = {
+    "Gowen - Powell": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Goulet - Pacheco": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Rooney - Hendricks": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Lawler - Smith": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Koenig - Fink": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Grippa - Curtain": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "McHugh - Polemeni": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Langdon - Doogan": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    },
+    "Palmer - Gru": {
+        "wins": 0,
+        "loses": 0,
+        "points": 0
+    }
+}
---
+++ assets/js/schedule.js
@@ -0,0 +1,274 @@
+schedule = {
+    "week 1": {
+        "game 1": {
+            "team 1": "Gowen - Powell",
+            "team 2": "Goulet - Pacheco",
+            "winner": "Gowen - Powell",
+            "loser": "Goulet - Pacheco",
+            "extra": -1
+        },
+        "game 2": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "Lawler - Smith",
+            "winner": "Lawler - Smith",
+            "loser": "Rooney - Hendricks",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Koenig - Fink",
+            "team 2": "Grippa - Curtain",
+            "winner": "Koenig - Fink",
+            "loser": "Grippa - Curtain",
+            "extra": 1
+        },
+        "game 4": {
+            "team 1": "McHugh - Polemeni",
+            "team 2": "Langdon - Doogan",
+            "winner": "Langdon - Doogan",
+            "loser": "McHugh - Polemeni",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Palmer - Gru",
+            "team 2": "Gowen - Powell",
+            "winner": "Gowen - Powell",
+            "loser": "Palmer - Gru",
+            "extra": 0
+        },
+    },
+
+    "week 2": {
+        "game 1": {
+            "team 1": "Langdon - Doogan",
+            "team 2": "Goulet - Pacheco",
+            "winner": "Langdon - Doogan",
+            "loser": "Goulet - Pacheco",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "Koenig - Fink",
+            "winner": "Rooney - Hendricks",
+            "loser": "Koenig - Fink",
+            "extra": -1
+        },
+        "game 3": {
+            "team 1": "Gowen - Powell",
+            "team 2": "Grippa - Curtain",
+            "winner": "Grippa - Curtain",
+            "loser": "Gowen - Powell",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Goulet - Pacheco",
+            "team 2": "Lawler - Smith",
+            "winner": "Lawler - Smith",
+            "loser": "Goulet - Pacheco",
+            "extra": -1
+        },
+        "game 5": {
+            "team 1": "Palmer - Gru",
+            "team 2": "McHugh - Polemeni",
+            "winner": "McHugh - Polemeni",
+            "loser": "Palmer - Gru",
+            "extra": 0
+        },
+    },
+
+    "week 3": {
+        "game 1": {
+            "team 1": "Koenig - Fink",
+            "team 2": "Lawler - Smith",
+            "winner": "Koenig - Fink",
+            "loser": "Lawler - Smith",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "Goulet - Pacheco",
+            "winner": "Goulet - Pacheco",
+            "loser": "Rooney - Hendricks",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "Langdon - Doogan",
+            "winner": "Grippa - Curtain",
+            "loser": "Langdon - Doogan",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Palmer - Gru",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Gowen - Powell",
+            "team 2": "Lawler - Smith",
+            "winner": "Lawler - Smith",
+            "loser": "Gowen - Powell",
+            "extra": 0
+        },
+    },
+
+    "week 4": {
+        "game 1": {
+            "team 1": "Gowen - Powell",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "Palmer - Gru",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "Rooney - Hendricks",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Koenig - Fink",
+            "team 2": "Goulet - Pacheco",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Langdon - Doogan",
+            "team 2": "Lawler - Smith",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+    },
+
+    "week 5": {
+        "game 1": {
+            "team 1": "Koenig - Fink",
+            "team 2": "Lawler - Smith",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Langdon - Doogan",
+            "team 2": "Gowen - Powell",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Goulet - Pacheco",
+            "team 2": "Palmer - Gru",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+    },
+
+    "week 6": {
+        "game 1": {
+            "team 1": "Goulet - Pacheco",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Rooney - Hendricks",
+            "team 2": "Langdon - Doogan",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "Lawler - Smith",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Langdon - Doogan",
+            "team 2": "Palmer - Gru",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Gowen - Powell",
+            "team 2": "Koenig - Fink",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+    },
+
+    "week 7": {
+        "game 1": {
+            "team 1": "Koenig - Fink",
+            "team 2": "McHugh - Polemeni",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 2": {
+            "team 1": "Lawler - Smith",
+            "team 2": "Palmer - Gru",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 3": {
+            "team 1": "Koenig - Fink",
+            "team 2": "Langdon - Doogan",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 4": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "Goulet - Pacheco",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 5": {
+            "team 1": "Grippa - Curtain",
+            "team 2": "Palmer - Gru",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+        "game 6": {
+            "team 1": "Gowen - Powell",
+            "team 2": "Rooney - Hendricks",
+            "winner": "",
+            "loser": "",
+            "extra": 0
+        },
+    }
+}
